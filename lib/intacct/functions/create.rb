28a5b1a37a898016da8f341c0f8850c6a736eea5
Yaroslav Konovets <yarikkonovets@gmail.com>
2022-09-27 12:55:19 +0300

    rework create func

--- lib/intacct/functions/create.rb
+++ lib/intacct/functions/create.rb
@@ -1,26 +1,20 @@
 module Intacct
   module Functions
     class Create
-      def initialize
-        @record_blocks = []
-      end
-
-      def add_record(record_type, &block)
-        @record_blocks << [record_type, block]
+      def initialize(record_type, &block)
+        @record_type = record_type
+        @block = block
       end
 
       def to_xml
         builder = Builder::XmlMarkup.new
 
         builder.create do
-          @record_blocks.each do |record_type, record_block|
-            builder.tag!(record_type) do
-              record_block.call(builder)
-            end
+          builder.tag!(@record_type) do
+            @block.call(builder)
           end
         end
       end
     end
   end
 end
-
