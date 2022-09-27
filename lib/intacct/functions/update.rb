8f3e69d3947538290a5123aab5a4af0ee9fc5ef0
Yaroslav Konovets <yarikkonovets@gmail.com>
2022-09-27 14:37:24 +0300

    rework Intacct::Functions::Update

--- lib/intacct/functions/update.rb
+++ lib/intacct/functions/update.rb
@@ -1,22 +1,17 @@
 module Intacct
   module Functions
     class Update
-      def initialize
-        @record_blocks = []
-      end
-
-      def add_record(record_type, &block)
-        @record_blocks << [record_type, block]
+      def initialize(record_type, &block)
+        @record_type = record_type
+        @block = block
       end
 
       def to_xml
         builder = Builder::XmlMarkup.new
 
         builder.update do
-          @record_blocks.each do |record_type, record_block|
-            builder.tag!(record_type) do
-              record_block.call(builder)
-            end
+          builder.tag!(@record_type) do
+            @block.call(builder)
           end
         end
       end
