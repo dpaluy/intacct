8a66721690e2dbaf0ce703e6ee5232b16f1bd009
Yaroslav Konovets <yarikkonovets@gmail.com>
2022-10-18 15:18:20 +0300

    add-create-aradjustment-function

---
+++ lib/intacct/functions/create_ar_adjustment.rb
@@ -0,0 +1,17 @@
+module Intacct
+  module Functions
+    class CreateArAdjustment
+      def initialize(&block)
+        @block = block
+      end
+
+      def to_xml
+        builder = Builder::XmlMarkup.new
+
+        builder.create_aradjustment do
+          @block.call(builder)
+        end
+      end
+    end
+  end
+end
