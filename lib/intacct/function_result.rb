c5e46ca410e5634449dc000a316cb4ffab888700
Mike Parris <mjp7290@gmail.com>
2023-09-27 10:40:29 -0700

    New method to return formatted api response error messages

--- lib/intacct/function_result.rb
+++ lib/intacct/function_result.rb
@@ -13,8 +13,18 @@ module Intacct
     end
 
     def parsed_data
-      hash = Crack::XML.parse(@xml_data.to_s)
+      hash = Crack::XML.parse(@xml_data.to_s).with_indifferent_access
       successful? ? hash.fetch('result') : hash
+    end
+
+    def push_error_messages
+      error_details = parsed_data.dig(:result, :errormessage, :error)
+
+      error_details = [error_details] unless error_details.is_a?(Array)
+
+      error_details.compact.map do |error_data|
+        [error_data[:description2], error_data[:correction]].compact.join(' ')
+      end
     end
   end
 end
