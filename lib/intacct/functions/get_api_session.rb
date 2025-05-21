e2ad4a3961494167dfd57b3872dcd8a2c82a4c8f
Yaroslav Konovets <yarikkonovets@gmail.com>
2025-05-21 14:19:59 -0500

    allow Intacct::Functions::GetApiSession to receive optional location_id

--- lib/intacct/functions/get_api_session.rb
+++ lib/intacct/functions/get_api_session.rb
@@ -1,10 +1,20 @@
 module Intacct
   module Functions
     class GetApiSession
+      def initialize(location_id: nil)
+        @location_id = location_id
+      end
+
       def to_xml
         builder = Builder::XmlMarkup.new
 
-        builder.getAPISession
+        if @location_id.present?
+          builder.getAPISession do
+            builder.locationid @location_id
+          end
+        else
+          builder.getAPISession
+        end
       end
     end
   end
